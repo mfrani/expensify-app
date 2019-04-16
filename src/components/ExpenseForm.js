0ca6abae62b11b19725388c77302da6ee9cf7eed
mfrani <matt.frani@gmail.com>
2019-04-16 16:32:08 -0700

    wired add/edit expense page

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -8,14 +8,18 @@ const now = moment();
 console.log(now.format("MMM Do, YYYY"));
 
 export default class ExpenseForm extends React.Component {
-  state = {
-    description: "",
-    note: "",
-    amount: "",
-    createdAt: moment(),
-    calendarFocused: false,
-    error: ""
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      description: props.expense ? props.expense.description : "",
+      note: props.expense ? props.expense.note : "",
+      amount: props.expense ? (props.expense.amount / 100).toString() : "",
+      createdAt: props.expense ? moment(props.expense.createdAt) : moment(),
+      calendarFocused: false,
+      error: ""
+    };
+  }
+
   onDescriptionChange = e => {
     const description = e.target.value;
     this.setState(() => ({
@@ -55,13 +59,18 @@ export default class ExpenseForm extends React.Component {
     } else {
       //clear error
       this.setState(() => ({ error: "" }));
-      console.log("submitted");
+      this.props.onSubmit({
+        description: this.state.description,
+        amount: parseFloat(this.state.amount, 10) * 100,
+        createdAt: this.state.createdAt.valueOf(),
+        note: this.state.note
+      });
     }
   };
   render() {
     return (
       <div>
-        <p>{this.state.error !== "" && this.state.error}</p>
+        {this.state.error && <p>{this.state.error}</p>}
         <form onSubmit={this.onSubmit}>
           <input
             type="text"
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,26 +1,18 @@
 import React from "react";
-import { removeExpense } from "../actions/expenses";
-import { connect } from "react-redux";
+
+import { Link } from "react-router-dom";
 //Export a statless functional component
 // description, amount, createdAt
 
-const ExpenseListItem = (
-  { dispatch, id, description, amount, createdAt },
-  props
-) => (
+const ExpenseListItem = ({ id, description, amount, createdAt }, props) => (
   <div>
-    <h3>{description}</h3>
+    <Link to={`/edit/${id}`}>
+      <h3>{description}</h3>
+    </Link>
     <p>
       {amount} - {createdAt}
     </p>
-    <button
-      onClick={() => {
-        dispatch(removeExpense({ id }));
-      }}
-    >
-      Remove
-    </button>
   </div>
 );
 
-export default connect()(ExpenseListItem);
+export default ExpenseListItem;
